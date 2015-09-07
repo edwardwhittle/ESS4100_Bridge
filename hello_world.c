6d46645be11367f7d8096ad2b864fa65cb41c2d6
Edward Whittle <edward.whittle1@live.vu.edu.au>
2015-09-07 22:39:11 +1000

    Connection Successful but Read Register Timed Out

--- hello_world.c
+++ hello_world.c
@@ -3,13 +3,13 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <errno.h>
 #define SERVER_ADDR "140.159.153.159"
 #define SERVER_PORT 502
 
-unsigned short errno;
-
-static void client(int count){
-}
+uint16_t tab_reg[64];
+int rc;
+int i;
 
 
 int main(void) {
@@ -27,8 +27,32 @@ modbus_t *ctx;
 		return -1;
 	}
 	else{
-		fprintf(stderr, "Connection Successful\n");
+		fprintf(stderr, "Libmodbus Context Created\n");
+	}
+/*Connect to Server*/
+	if (modbus_connect(ctx) == -1) {
+	               fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+		       modbus_free(ctx);
+			return -1;						               
 	}
 
-    	return 0;
+	else{
+		fprintf(stderr, "Connection Succesful\n");
+	}
+
+/*Read Registers*/
+	rc = modbus_read_registers(ctx, 0, 3,tab_reg);
+	   	if (rc == -1) {
+			fprintf(stderr, "Read Register Failed: %s\n", modbus_strerror(errno));
+			return -1;
+		}
+
+		for (i=0; i < rc; i++) {
+			printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
+		}
+
+	modbus_close(ctx);
+	modbus_free(ctx);
+return 0;
+
 }
